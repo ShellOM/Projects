78585f2d9cef76c69f57ad2abb426f56d9154a36
ShellOM <147545294+ShellOM@users.noreply.github.com>
2023-11-05 17:06:38 +0000

    JS coding completed.

--- Hangman.js
+++ Hangman.js
@@ -27,3 +27,53 @@ function updateWordDisplay() {
 function updateLives() {
     document.getElementById("lives").lextContent = lives;
 }
+
+// Checks if the player won the game
+function checkWin() {
+    if (wordDisplay.join("") === selectWord) {
+        document.getElementById("message").textContent = "Congratulations! You win!";
+        document.getElementById("guess-input").style.display = "none";
+    }
+}
+
+// Checks if the player has lost
+function checkLose() {
+    if (lives <= 0) {
+        document.getElementById("message").textContent = "Game Over! The word was" + selectWord;
+        document.getElementById("guess-input").style.display = "none";
+    }
+}
+
+// This will handle the user guesses
+document.getElementById("guess-button").addEventListener("click", function () {
+    const letterInput = document.getElementById("letter-input").value.toUpperCase();
+
+    if (letterInput.length === 1 && selectWord.includes(letterInput)) {
+        //If the letter is in the word
+        for (let i = 0; i < selectWord.length; i++) {
+            if (selectWord[i] === letterInput) {
+                wordDisplay[i] = letterInput;
+            }
+        }
+        updateWordDisplay();
+        checkWin();
+    } else if (letterInput.length > 1 && letterInput === selectWord) {
+        // When the whole word is guessed
+        wordDisplay = selectWord.split("");
+        updateWordDisplay();
+        checkWin();
+    } else {
+        //If the guess is incorrect
+        if (!guesses.includes(letterInput)) {
+            guesses.push(letterInput);
+            lives--;
+            updateLives();
+            checkLose();
+        }
+    }
+    document.getElementById("letter-input").value = ""; //Clears the input field
+});
+
+//Initiates the game 
+updateWordDisplay();
+updateLives();
